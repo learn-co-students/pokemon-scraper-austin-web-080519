eb2a634fa3c1742ad11e77b1f0d005ff4f527b8b
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-08-15 11:18:47 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,10 @@
 class Pokemon
-
+    #@@database_connection = SQLite3::Database.new('db/pokemon.db')
+    #@@database_connection.execute('CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);')
+    #^creates the table
     attr_accessor :id, :name, :type, :db
 
+    #universal method to take in attributes from something like an API
     def initialize(attributes)
         attributes.each {|key, value| self.send(("#{key}="), value)}
     end
@@ -16,4 +19,9 @@ class Pokemon
         Pokemon.new(id:pokemon_extraction[0], name:pokemon_extraction[1], type:pokemon_extraction[2], db:db)
     end
 
+    def change_name(name, db)
+        db.execute("UPDATE pokemon SET name = '?' WHERE id = ? ", name, self.id)
+    end
+
+    #Pikachu.change_name("JigglyPuff", pokemon)
 end
